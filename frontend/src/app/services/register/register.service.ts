94fdecc6e90178758a458b303306039329f784d7
meriem52 <MARIEM.LOGTARI@insat.ucar.tn>
2024-08-02 21:11:09 +0100

    Add register feature

---
+++ frontend/src/app/services/register/register.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  private apiUrl = 'http://localhost:3001/api/auth';
+
+  constructor(private http: HttpClient) { }
+
+  register(username: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, { username, password }, { withCredentials: true });
+  }
+}
