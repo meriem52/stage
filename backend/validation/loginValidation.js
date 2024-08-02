0b4626eac3a746a6953aec46e25fc7777ec36ac6
meriem52 <MARIEM.LOGTARI@insat.ucar.tn>
2024-08-02 21:17:46 +0100

    Add login feature

---
+++ backend/validation/loginValidation.js
@@ -0,0 +1,7 @@
+const Joi = require('joi');
+const schema = Joi.object({
+    username: Joi.string().alphanum().min(4).max(30).required(),
+    password: Joi.string().min(5).required(),
+}).unknown(false); // Disallow additional properties
+const loginSchema = () => schema;
+module.exports = {  loginSchema };
